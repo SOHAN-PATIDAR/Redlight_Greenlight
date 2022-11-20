148e2618230eece736d045e3440d393419038112
Sohan Patidar <patelsohan1005@gmail.com>
2022-11-20 09:57:45 +0530

    Adding Doll,Player and player functionality

--- js/main.js
+++ js/main.js
@@ -16,9 +16,36 @@ directionalLight.position.set( 0, 1, 1 )
 camera.position.z = 5
 renderer.setClearColor( 'wheat')
 
+const loader = new THREE.GLTFLoader()
+let doll
+
+const start_position = 6
+const end_position = -start_position
+
 const text = document.querySelector('.text')
 
+let DEAD_PLAYERS = 0
+let SAFE_PLAYERS = 0
+
 const startBtn = document.querySelector('.start-btn')
+
+loader.load( './model/scene.gltf', function ( gltf ){
+    scene.add( gltf.scene )
+    doll = gltf.scene
+    gltf.scene.position.set(0,.1, -.1)
+    gltf.scene.scale.set(0.3, 0.25, 0.3)
+    startBtn.innerText = "start"
+})
+
+function lookBackward(){
+    gsap.to(doll.rotation, {duration: .45, y: -3.15})
+    setTimeout(() => dallFacingBack = true, 150)
+    //light1=lights('red')
+}
+function lookForward(){
+    gsap.to(doll.rotation, {duration: .45, y: 0})
+    setTimeout(() => dallFacingBack = false, 450)
+}
 
 function createCube(size, posX, rotY = 0, color = 0xfbc851){
     const geometry = new THREE.BoxGeometry( size.w, size.h, size.d )
@@ -35,6 +62,92 @@ createCube({w: start_position * 2 + .21, h: 1.5, d: 1}, 0, 0, 0xe5a716).position
 createCube({w: .2, h: 1.5, d: 2}, start_position, -.4)
 createCube({w: .2, h: 1.5, d: 2}, end_position, .4)
 
+class Player {
+    constructor(name = "Player", radius = .25, posY = 0, color = 0xffffff){
+        const geometry = new THREE.SphereGeometry( radius, 100, 100 )
+        const material = new THREE.MeshBasicMaterial( { color } )
+        const player = new THREE.Mesh( geometry, material )
+        scene.add( player )
+        player.position.x = start_position - .4
+        player.position.z = 1
+        player.position.y = posY
+        this.player = player
+        this.playerInfo = {
+            positionX: start_position - .4,
+            velocity: 0,
+            name,
+            isDead: false
+        }
+    }
+
+    run(){
+        if(this.playerInfo.isDead) return
+        this.playerInfo.velocity = .03
+    }
+
+    stop(){
+        gsap.to(this.playerInfo, { duration: .1, velocity: 0 })
+    }
+
+    check(){
+        if(this.playerInfo.isDead) return
+        if(!dallFacingBack && this.playerInfo.velocity > 0){
+            text.innerText = this.playerInfo.name + " is Dead."
+            this.playerInfo.isDead = true
+            this.stop()
+            DEAD_PLAYERS++
+            
+            if(DEAD_PLAYERS == players.length){
+                text.innerText = "No one Survived!"
+                gameStat = "ended"
+            }
+            if(DEAD_PLAYERS + SAFE_PLAYERS == players.length){
+                gameStat = "ended"
+            }
+        }
+        if(this.playerInfo.positionX < end_position + .7){
+            text.innerText = this.playerInfo.name + " Survived!!!"
+            this.playerInfo.isDead = true
+            this.stop()
+            SAFE_PLAYERS++
+            
+            if(SAFE_PLAYERS == players.length){
+                text.innerText = "Everyone Survived!!!"
+                gameStat = "ended"
+            }
+            if(DEAD_PLAYERS + SAFE_PLAYERS == players.length){
+                gameStat = "ended"
+            }
+        }
+    }
+
+    update(){
+        this.check()
+        this.playerInfo.positionX -= this.playerInfo.velocity
+        this.player.position.x = this.playerInfo.positionX
+    }
+}
+
+async function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const player1 = new Player("Player 1", .25, .3, 'green')
+const player2 = new Player("Player 2", .25, -.3, 'yellowgreen')
+
+const players = [
+    {
+        player: player1,
+        key: "ArrowUp",
+        name: "Player 1"
+    },
+    {
+        player: player2,
+        key: "w",
+        name: "Player 2"
+    }
+]
+
 const TIME_LIMIT = 17
 async function init(){
     await delay(500)
@@ -46,8 +159,35 @@ async function init(){
     lookBackward()
     await delay(500)
     text.innerText = "Gooo!!!"
-    bgMusic.play()
     start()
+}
+
+
+let gameStat = "loading"
+
+function start(){
+    gameStat = "started"
+    const progressBar = createCube({w: 8, h: .1, d: 1}, 0, 0, 0xebaa12)
+    progressBar.position.y = 3.35
+    gsap.to(progressBar.scale, {duration: TIME_LIMIT, x: 0, ease: "none"})
+    setTimeout(() => {
+        if(gameStat != "ended"){
+            text.innerText = "Time Out!!!"
+            
+            gameStat = "ended"
+            //lights.change()
+        }
+    }, TIME_LIMIT * 1000)
+    startDall()
+}
+
+let dallFacingBack = true
+async function startDall(){
+   lookBackward()
+   await delay((Math.random() * 1500) + 1500)
+   lookForward()
+   await delay((Math.random() * 750) + 750)
+   startDall()
 }
 
 startBtn.addEventListener('click', () => {
@@ -65,6 +205,20 @@ function animate(){
 }
 animate()
 
+window.addEventListener( "keydown", function(e){
+    if(gameStat != "started") return
+    let p = players.find(player => player.key == e.key)
+    if(p){
+        p.player.run()
+    }
+})
+
+window.addEventListener( "keyup", function(e){
+    let p = players.find(player => player.key == e.key)
+    if(p){
+        p.player.stop()
+    }
+})
 
 window.addEventListener( 'resize', onWindowResize, false )
 function onWindowResize(){
